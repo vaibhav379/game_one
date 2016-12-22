7c8c43778bb65d1e87f37dbcc62ba6345d5c0b28
vaibhav379 <vaibhavmalhotra17@gmail.com>
2016-12-22 22:29:25 +0530

    baserepeat

---
+++ Assets/BaseRepeat.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseRepeat : MonoBehaviour {
+
+
+	Vector3 place = new Vector3(12,-4,0);
+
+	public GameObject base1;
+
+	// Use this for initialization
+	void Start () {
+	
+		InvokeRepeating ("inst", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+		//transform.Translate (5*Time.deltaTime,0,0);
+
+	}
+
+	void inst (){
+		
+		Instantiate (base1,place,transform.rotation);
+		place.x += 8;
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed =5f,jumpspeed=2f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			transform.Translate (speed * Time.deltaTime, 0, 0);
+
+		if (Input.GetButtonDown ("Jump")) {
+			
+		//	transform.Translate (0, jump * Time.deltaTime,0);
+
+		
+
+		} 
+
+	}
+
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		
+
+		if(col.gameObject.tag=="obstacle")
+		Destroy (gameObject);
+	}
+
+}
